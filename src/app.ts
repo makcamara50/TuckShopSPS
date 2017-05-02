eab3de1cd859048b24bef89816c92d462d1ed1bc
RyanRio <thermitchellman@gmail.com>
2017-05-02 16:44:21 -0400

    COMMIT

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import Rx from 'rxjs/Rx';
+
+
+
+class ryan1 {
+    name:string
+    constructor () {
+        this.name="hi"
+    }
+}
+function ryan() {
+    let name: string = "hi"
+    const that = {
+        name
+    }
+    return that
+}
+
+let a = ryan()
+console.log(a)
+let b = new ryan1()
+console.log(b.name)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 module.exports = {
-	entry: './src/app.js',
+	entry: './src/app.ts',
 	output: {
 		path: './dist',
 		filename:'app.bundle.js'
 	},
 	module: {
-		loaders: [{
-			exclude: '/node_modules/',
-			loader: 'babel-loader',
-			query: {
-				presets: ['es2015']
-			}
-		}]
-	}
+    loaders: [{
+      // The loader that handles ts and tsx files.  These are compiled
+      // with the ts-loader and the output is then passed through to the
+      // babel-loader.  The babel-loader uses the es2015 and react presets
+      // in order that jsx and es6 are processed.
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?presets[]=es2015!ts-loader'
+    }, {
+      // The loader that handles any js files presented alone.
+      // It passes these to the babel-loader which (again) uses the es2015
+      // and react presets.
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }
+	]
+}
 }
